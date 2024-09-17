d8c76d19d47b0fbb62b9996ff340424f86b7c16c
pablo-sa10 <pablomoura2016@gmail.com>
2024-09-17 10:41:38 -0300

    apreendendo laravel

---
+++ projetobd/app/Http/Controllers/Series.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers;
+use Illuminate\Http\Request;
+
+class Series extends Controller
+{
+ 
+    public function index(Request $request){
+
+        //return redirect('https://google.com');
+        $series = [
+            'the walking dead',
+            'game of thrones',
+            'dark'
+        ];
+    
+        return view('listar-series', compact('series'));
+    }
+}
